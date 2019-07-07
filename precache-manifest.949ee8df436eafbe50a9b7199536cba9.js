de68bf1ea2f9925883aecebe97d4116c14ad6c7c
Kamil Nowak <info@kamilnowak.com>
2019-07-07 16:15:30 +0200

    Updates

---
+++ precache-manifest.949ee8df436eafbe50a9b7199536cba9.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82ec27a813f11f1bef1f0b8340b8040f",
+    "url": "/a108-crypto-rate/index.html"
+  },
+  {
+    "revision": "9cd94354f3b67f84edf7",
+    "url": "/a108-crypto-rate/static/css/2.627d0f56.chunk.css"
+  },
+  {
+    "revision": "ca51cf6fe2056ac8cdf5",
+    "url": "/a108-crypto-rate/static/css/main.d27f95ac.chunk.css"
+  },
+  {
+    "revision": "9cd94354f3b67f84edf7",
+    "url": "/a108-crypto-rate/static/js/2.31cefde2.chunk.js"
+  },
+  {
+    "revision": "ca51cf6fe2056ac8cdf5",
+    "url": "/a108-crypto-rate/static/js/main.15d26b75.chunk.js"
+  },
+  {
+    "revision": "efd252e7003313dc2ce3",
+    "url": "/a108-crypto-rate/static/js/runtime~main.84bd00f7.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/a108-crypto-rate/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/a108-crypto-rate/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/a108-crypto-rate/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/a108-crypto-rate/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/a108-crypto-rate/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
